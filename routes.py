fca1e43cb3aba8cc71e9d83b1dcd53257f98341f
Jonathan Williamson <jonathan@cgcookie.com>
2014-12-10 19:23:24 -0600

    Add initial signup page, navigation, and route

--- routes.py
+++ routes.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
  
 @app.route('/')
 def home():
-  return render_template('home.html')
- 
+	return render_template('home.html')
+
+@app.route('/signup')
+def signup():
+	return render_template('signup.html')
+
 if __name__ == '__main__':
-  app.run(debug=True)
+	app.run(debug=True)
